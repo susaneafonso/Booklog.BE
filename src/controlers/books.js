ab0794757d7a05b3f6e932674662ec979d52d42b
susaneafonso <susaneafonso@users.noreply.github.com>
2024-01-08 08:38:18 -0300

    .

--- src/controlers/books.js
+++ src/controlers/books.js
@@ -1,32 +1,72 @@
 const {conex} = require ("../database.js")
-
 const mysql= require('mysql2')
 
+////////get///////////////////////////////
 async function get_book(req,res){
 try{
     const connection= await conex()
     const[rows]= await connection.query("Select * from books")
-    return res.status(200).json(rows).send()
+    return res.status(200).json(rows)
 }
 catch(error){
     console.log(error)
-    res.status(500).json({error:"preciso arrumar o erro"}).send()
+    return res.status(500).json({error:"preciso arrumar o erro"})
 }
 }
-
+///////////post//////////////////////////////////
 async function add_book(req,res){
-const connection= await conex()
-const data= req.body
+
+    console.log(req.body)
+    const {ttle_bk,auth_bk,genre_bk,date_publish}=req.body
+    const values=[ttle_bk,auth_bk,genre_bk,date_publish]
 try{
-const{ttle_bk, auth_bk,genre_bk, date_publish}=data
-const result= await connection.query("insert into books(ttle_bk, auth_bk,genre_bk, date_publish)values(?,?,?,?)",[ttle_bk, auth_bk,genre_bk, date_publish])
-return res.status(200).json({message:"livro registrado!"}).send()
+    if (values.some(value => value === null || value === undefined)) {
+        return res.status(400).json({ error: "Todos os campos são obrigatórios." ,requestBody:values}).send()
+    }
+const connection= await conex()
+const sql='insert into books(ttle_bk, auth_bk,genre_bk,date_publish)values(?,?,?,?)'
+const result= await connection.query(sql,values)
+return res.status(200).json({message:"livro registrado!",requestBody: data}).send()
 }
 catch(error){
 console.error(error)
 res.status(500).json({ error: 'Internal Server Error' }).send() 
 }
 }
-module.exports={add_book,get_book}
+//////////////////    delete ////////////////////////////////
+async function del_book(req,res){
+const {id_bk}= req.params
+try{
+const connection=await conex()
+const result= await connection.query("Delete from books where id_bk=?",[id_bk])
+return res.status(200).json({message:"Livro removido!"}).send()
+}
+catch(error){
+    console.log(error)
+return res.status(500).json({Error:"Internal Server Error"}.send)
+}
+}
+//////////////////////// update///////////////////////////
+async function up_book(req,res){
+const {id_bk}= req.params
+const data=req.body   
+try{
+if(!ttle_bk || !auth_bk || !genre_bk || !date_publish){
+const{ttle_bk, auth_bk,genre_bk, date_publish}=data
+const connection= await conex()
+const result= await connection.query("update books set ttle_bk=?, auth_bk=?,genre_bk=?, date_publish=?  where id_bk=?",[ttle_bk, auth_bk,genre_bk, date_publish,id_bk])
+console.log(req.body)
+return res.status(200).json({message:"Registro Alterado!",requestBody: data}).send()}
+} 
+catch(error){
+console.log(error)
+return res.status(500).json({error:"Internal Server Error"}).send()
+}
+
+}
+
+
+
+module.exports={add_book,get_book,del_book,up_book}
 
     
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,29 @@
 const express= require('express')
 const controler_books=require('./controlers/books')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const app = express()
 
 const routes= express.Router()
 routes.post("/books",controler_books.add_book)
 routes.get("/books",controler_books.get_book)
+routes.delete("/books/:id_bk",controler_books.del_book)
+routes.put("/books/:id_bk",controler_books.up_book)
 
 app.use(cors())
 app.use(express.json())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+app.use((req, res, next) => {
+  console.log(req.method, req.url, req.body);
+  next()
+})
 app.use('/api',routes)
+
 module.exports=app
 
-app.listen(5000, () => {
+app.listen(5500, () => {
   console.log("Server is running on port ")
 })
